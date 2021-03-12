eac1dc5588e10d792d472a7141713403bbd7522b
Ryan <ryanxgraham@gmail.com>
2021-03-12 16:57:02 -0600

    stubbed out connection.py

--- sync/connection.py
+++ sync/connection.py
@@ -2,14 +2,42 @@ from abc import ABC, abstractmethod
 
 
 class AbstractConnection(ABC):
+    def __init__(self):
+        pass
+
     @abstractmethod
     def connect(self):
         pass
     
     @abstractmethod
-    def close(self):
+    def cursor(self):
         pass
 
     @abstractmethod
-    def cursor(self):
+    def insert(self, row):
         pass
+
+    @abstractmethod
+    def delete(self, row):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def create_table(self, table_name):
+        pass
+
+    @abstractmethod
+    def get_table(self, table_name):
+        pass
+
+    @abstractmethod
+    def drop_table(self, table_name):
+        pass
+
+    @abstractmethod
+    def export(self, table_name):
+        pass
+    
