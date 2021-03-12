2d0b9daff3b02fbacebc4f5753f9460915acd8f9
Ryan <ryanxgraham@gmail.com>
2021-03-12 13:48:41 -0600

    added class files

--- sync/connection.py
+++ sync/connection.py
@@ -13,24 +13,3 @@ class AbstractConnection(ABC):
     @abstractmethod
     def cursor(self):
         pass
-
-    def __enter__(self):
-        self.connect()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    @abstractmethod
-    @classmethod
-    def from_config(cls, config):
-        pass
-
-    @abstractmethod
-    def load(self):
-        pass
-
-    @abstractmethod
-    def unload(self):
-        pass
-
-    
